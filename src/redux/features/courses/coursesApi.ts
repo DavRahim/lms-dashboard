3f7320479c893d88d60b8f8e15eabe53b9b65bcb
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-08-19 19:33:44 +0600

    create course redux add

---
+++ src/redux/features/courses/coursesApi.ts
@@ -0,0 +1,17 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const courseApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        createCourse: builder.mutation({
+            query: (data) => ({
+                url: "create-course",
+                method: "POST",
+                body: data,
+                credentials: "include" as const
+            })
+        }),
+    })
+})
+
+
+export const { useCreateCourseMutation } = courseApi    
